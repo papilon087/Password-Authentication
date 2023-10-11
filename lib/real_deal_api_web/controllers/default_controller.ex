7302f53d2a757c562fbdb687df0dfbe43ab905ce
papilon087 <ademirgus@gmail.com>
2023-10-11 14:20:41 -0300

    feat: config default

--- lib/real_deal_api_web/controllers/default_controller.ex
+++ lib/real_deal_api_web/controllers/default_controller.ex
@@ -2,6 +2,6 @@ defmodule RealDealApiWeb.DefaultController do
   use RealDealApiWeb, :controller
 
   def index(conn, _params) do
-    text conn, "The Real Deal API is Live - #{Mix.env()}"
+    text(conn, "The Real Deal API is Live - #{Mix.env()}")
   end
 end
