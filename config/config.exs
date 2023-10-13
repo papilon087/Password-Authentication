6df6ed9cef8290148d404b0e219e5b2449c0b65c
papilon087 <ademirgus@gmail.com>
2023-10-12 21:22:31 -0300

    feat: using guardian and bcrypt

--- config/config.exs
+++ config/config.exs
@@ -23,6 +23,10 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :real_deal_api, RealDealApiWeb.Auth.Guardian,
+  issuer: "real_deal_api",
+  secret_key: "PIkDyKZsO9gnXxMpJn3YZq9rwJ4bn3/wG4YqL/kSx7Fe4DPKy6sq/cFIwm5/2UPc"
+
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
